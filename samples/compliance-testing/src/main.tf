069fa1d2e38055ee826fb563bef004677f701f7f
Preeti8600 <33160356+Preeti8600@users.noreply.github.com>
2022-06-06 16:13:32 +0530

    Update main.tf

--- samples/compliance-testing/src/main.tf
+++ samples/compliance-testing/src/main.tf
@@ -4,8 +4,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-hello-tf-${random_uuid.uuid.result}"
   location = var.location
 
+  
   tags = {
     Environment = "dev"
+   
     Application = "Azure Compliance"
   } 
 }
